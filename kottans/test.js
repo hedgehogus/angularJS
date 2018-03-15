0871001082d5e04899349b88297769a9c0dea5b2
hedgehogus <research162@gmail.com>
2018-03-15 22:03:34 +0200

    two components

--- kottans/test.js
+++ kottans/test.js
@@ -1,5 +1,17 @@
 angular
     .module('kottans',[])
+    .component('sum', {
+        templateUrl: 'sum.html',
+
+        controller($scope){
+            this.firstValue=1;
+            this.secondValue=2;
+
+            $scope.$watchCollection('[$ctrl.firstValue, $ctrl.secondValue]', () => {
+                this.sum = +this.firstValue + +this.secondValue;
+            })
+        }
+    })
     .component('numberInput',{
         template:[
             '<input type="text" ng-model="$ctrl.value" />',
@@ -12,11 +24,21 @@ angular
             value: '='
         },
 
-        controller(){
-            this.isTimerOn = false;
+        controller($scope, $interval){
+            // this.isTimerOn = false;
             this.step = (step) => this.value = +(this.value) + step;
 
-            this.toggleTimer = () => {this.isTimerOn = !this.isTimerOn;}
+            // let timer;
+            // this.toggleTimer = () => {
+            //     this.isTimerOn = !this.isTimerOn;
+            //     if(timer){
+            //         $interval.cancel(timer);
+            //     } else {
+            //         timer = $interval(() => {
+            //             this.value = +(this.value) + 1;                       
+            //         }, 1000);
+            //     }
+            // }
         }
     }
 )
