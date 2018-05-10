bc9164b8453a10347fb2ed746c9950ffc4baa6fe
hedgehogus <research162@gmail.com>
2018-05-11 00:06:34 +0300

    http

--- angularJs Codeschool2/javascript/controllers/notes-index-controller.js
+++ angularJs Codeschool2/javascript/controllers/notes-index-controller.js
@@ -1,6 +1,5 @@
 angular.module('NoteWrangler').controller('NotesIndexController', function($http){
-    var controller = this;
-    console.log(controller.notes);
+    var controller = this;    
      $http({method: 'GET', url: 'templates/pages/notes/data.json'}).then(function(resp){
          controller.notes = resp.data;
          console.log(controller.notes);
