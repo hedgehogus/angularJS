392a0b7a0837507e4f2b06c1e60c4c72e887a7f4
hedgehogus <research162@gmail.com>
2018-05-29 00:18:01 +0300

    c

--- angularJs Codeschool2/javascript/directives/nw-card.js
+++ angularJs Codeschool2/javascript/directives/nw-card.js
@@ -2,8 +2,9 @@ angular.module('NoteWrangler').directive("nwCard", function(){
     return {        
         restrict: 'E',
         templateUrl: "templates/pages/notes/nw-card.html",
-        controller: function(){
-            this.header = "note title"
+        controller: function($scope){
+            this.header = "note title";
+            $scope.header = "from scope";
         },
         controllerAs: "card"
     }
