4c59c285d182618aa37740cdc9e283ee65484f57
hedgehogus <research162@gmail.com>
2018-05-30 00:31:41 +0300

    input

--- angularJs Codeschool2/javascript/directives/nw-card.js
+++ angularJs Codeschool2/javascript/directives/nw-card.js
@@ -1,10 +1,18 @@
-angular.module('NoteWrangler').directive("nwCard", function(){    
-    return {        
+angular.module('NoteWrangler').directive("nwCard", function(){   
+    var num = 0; 
+    return {       
         restrict: 'E',
         templateUrl: "templates/pages/notes/nw-card.html",
+        scope: {note: '=' 
+           // note: "@" //input
+            // note: '=' // two way binding
+        }, // child no longer has access to the parents scope
         controller: function($scope){
             this.header = "note title";
-            $scope.header = "from scope";
+            $scope.header = "from scope ";
+            num++;
+            $scope.num = num;
+            console.log($scope);
         },
         controllerAs: "card"
     }
