ead120c33f888f8192e6e9b12bc9c84a1d7e8062
hedgehogus <research162@gmail.com>
2018-05-31 23:34:55 +0300

    comments

--- angularJs Codeschool2/javascript/directives/nw-card.js
+++ angularJs Codeschool2/javascript/directives/nw-card.js
@@ -4,9 +4,9 @@ angular.module('NoteWrangler').directive("nwCard", function(){
         restrict: 'E',
         templateUrl: "templates/pages/notes/nw-card.html",
         scope: {note: '=' 
-           // note: "@" //passes in a string
+           // note: "@" //passes in a string //passed directly or through interpolation with double curly braces notation
             // note: '=' // two way binding
-            // note: '&' //operator allows you to invoke or evaluate an expression on the parent scope of whatever the directive is inside of.
+            // note: '&' //someExpression //operator allows you to invoke or evaluate an expression on the parent scope of whatever the directive is inside of.
         }, // child no longer has access to the parents scope - isolate scope 
         controller: function($scope){
             this.header = "note title";
